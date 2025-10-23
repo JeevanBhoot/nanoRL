a43f6e59c050c97e98b4921f8a0693ca9c428c6a
JeevanBhoot <jeevan.bhoot@yahoo.com>
2025-10-23 20:36:06 +0100

    Add more to REINFORCE train script

--- easy_rl/train_reinforce.py
+++ easy_rl/train_reinforce.py
@@ -1,19 +1,27 @@
-from easy_rl.data import make_dataloader
-from easy_rl.models import HFPolicy
+import torch
+import torch.nn as nn
+import tqdm
+from torch.utils.data import DataLoader
+
+from dataclasses import dataclass
+
+from easy_rl.data import make_dataloader, TEST_TOPIC
+from easy_rl.hf_policy import HFPolicy
 from easy_rl.rewards import reward
 
-import torch
+@dataclass
+class TrainConfig:
+    model_id: str = "meta-llama/Llama-3.2-1B-Instruct"
+    batch_size: int = 8
+    learning_rate: float = 1e-4
+    num_epochs: int = 10
+    grad_clip: float = 1.0
 
-# CONFIG
-# dataclass or dict?
 
-def train_reinforce(config: dict):
-    policy = HFPolicy(config["model_id"])
-    dataloader = make_dataloader(config["batch_size"])
-    optimizer = torch.optim.Adam(policy.model.parameters(), lr=config["learning_rate"])
-
-    for epoch in range(config["num_epochs"]):
-        for batch in dataloader:
+def train_reinforce(policy: HFPolicy, dataloader: DataLoader, optimizer: torch.optim.Optimizer, config: TrainConfig) -> None:
+    for epoch in range(config.num_epochs):
+        for batch in tqdm.tqdm(dataloader, desc=f"Epoch {epoch+1}/{config.num_epochs}"):
+            optimizer.zero_grad()
             prompts = batch["prompts"]
             texts, logprobs = policy.generate_with_logprobs(prompts)
             rewards = reward(texts)
@@ -21,5 +29,24 @@ def train_reinforce(config: dict):
             # without baseline
             loss = -(logprobs * rewards).mean()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(policy.model.parameters(), config["grad_clip"])
+            nn.utils.clip_grad_norm_(policy.model.parameters(), config.grad_clip)
             optimizer.step()
+        print(f"Epoch {epoch+1}/{config.num_epochs} loss: {loss.item()}")
+        torch.save(policy.model.state_dict(), f"model_{epoch+1}.pth")
+        print(f"Model saved to model_{epoch+1}.pth")
+
+def main():
+    config = TrainConfig()
+    policy = HFPolicy(config.model_id)
+    test_text = policy.generate([TEST_TOPIC])
+    dataloader = make_dataloader(config.batch_size)
+    optimizer = torch.optim.AdamW(policy.model.parameters(), lr=config.learning_rate)
+    train_reinforce(policy, dataloader, optimizer, config)
+    test_text_after = policy.generate([TEST_TOPIC])
+    print("Test text before training:")
+    print(test_text)
+    print("Test text after training:")
+    print(test_text_after)
+
+if __name__ == "__main__":
+    main()
